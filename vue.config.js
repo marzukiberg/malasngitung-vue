a9bfa9fc978fea17a4f4ca0b3c7c16456c51584f
marzukiberg <marzukiberg@gmail.com>
2021-08-02 05:41:08 +0700

    icon and name changed

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   lintOnSave: false,
+  pwa: {
+    name: 'MalasNgitung',
+    themeColor: '#22252D',
+    msTileColor: '#22252D',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  },
 };

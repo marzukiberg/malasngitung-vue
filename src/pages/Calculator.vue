12789eb406f0d1cf1c429763d125abd432145ff5
marzukiberg <marzukiberg@gmail.com>
2021-08-04 03:07:29 +0700

    fix dot problems

--- src/pages/Calculator.vue
+++ src/pages/Calculator.vue
@@ -93,68 +93,66 @@
 </template>
 
 <script>
-const objectFromNumbers = ({ count = 3, startAt = 1 }) => {
-  return [...Array(count).keys()].map((item) => {
-    return {
-      text: item + startAt,
-      render: `<span>${item + startAt}</span>`,
-    };
-  });
-};
+const objectFromNumbers = ({ count = 3, startAt = 1 }) => [...Array(count).keys()].map((item) => ({
+  text: item + startAt,
+  render: `<span>${item + startAt}</span>`,
+}));
 
 const BUTTONS = [
-  { text: "ac", render: `<span class="text-green-500">CE</span>` },
+  { text: 'ac', render: '<span class="text-green-500">CE</span>' },
   {
-    text: "plusmin",
-    render: `<span class="text-green-500">+/-</span>`,
+    text: 'plusmin',
+    render: '<span class="text-green-500">+/-</span>',
   },
   {
-    text: "percent",
-    render: `<i class="fa fa-percent text-green-500" aria-hidden="true"></i>`,
+    text: 'percent',
+    render: '<i class="fa fa-percent text-green-500" aria-hidden="true"></i>',
   },
   {
-    text: "÷",
-    render: `<i class="fas fa-divide text-red-500"></i>`,
+    text: '÷',
+    render: '<i class="fas fa-divide text-red-500"></i>',
   },
   ...objectFromNumbers({
     startAt: 7,
   }),
   {
-    text: "x",
-    render: `<i class="fas fa-times text-red-500"></i>`,
+    text: 'x',
+    render: '<i class="fas fa-times text-red-500"></i>',
   },
   ...objectFromNumbers({
     startAt: 4,
   }),
   {
-    text: "-",
-    render: `<i class="fas fa-minus text-red-500"></i>`,
+    text: '-',
+    render: '<i class="fas fa-minus text-red-500"></i>',
   },
   ...objectFromNumbers({
     startAt: 1,
   }),
   {
-    text: "+",
-    render: `<i class="fas fa-plus text-red-500"></i>`,
+    text: '+',
+    render: '<i class="fas fa-plus text-red-500"></i>',
   },
   {
-    text: "reset",
-    render: `<i class="fas fa-redo transform -scale-x-100" aria-hidden="true"></i>`,
+    text: 'reset',
+    render:
+      '<i class="fas fa-redo transform -scale-x-100" aria-hidden="true"></i>',
   },
-  { text: "0", render: `<span>0</span>` },
-  { text: ".", render: `<span>&#x2022</span>` },
-  { text: "=", render: `<i class="fas fa-equals text-red-500"></i>` },
+  { text: '0', render: '<span>0</span>' },
+  { text: '.', render: '<span>&#x2022</span>' },
+  { text: '=', render: '<i class="fas fa-equals text-red-500"></i>' },
 ];
 
 export default {
-  name: "Calculator",
+  name: 'Calculator',
   created() {
-    document.title = "MalasNgitung";
+    document.title = 'MalasNgitung';
   },
   data() {
     return {
-      theme: "dark",
+      theme: 'light',
       buttons: BUTTONS,
+      operators: new RegExp(/[+|\-|x|\u÷]/g),
       state: {
         display: 0,
         calculation: 0,
@@ -164,95 +162,40 @@ export default {
     };
   },
   updated() {
-    console.log(this.$data.state);
+    // console.log(this.$data.state);
   },
   methods: {
     handleButton(e) {
-      String.prototype.replaceAt = function (index, replacement) {
-        if (index >= this.length) {
-          return this.valueOf();
-        }
-        return (
-          this.substring(0, index) + replacement + this.substring(index + 1)
-        );
-      };
       const text = e.target.dataset.text.toString();
-      const display = this.state.display.toString();
-
-      const lastIsDot = () => display.slice(-1) === ".";
-      const displayIsZero = () => display == "0";
-      const firstInput = display.charAt(0);
-      const firstTwoInput = display.substring(0, 2);
-      const lastInput = display.charAt(display.length - 1);
-      const operators = new RegExp(/[x|÷|+|-]/g);
-      const lastIsOperator = lastInput.match(operators);
-      const isOperatorIncluded = display
-        .substring(1, display.length)
-        .match(operators);
-      const includeDot = display.includes(".");
-      const operatorsCount = isOperatorIncluded?.length;
-      const removedScopedMinus = display.substring(2, display.length - 1);
+      const { display } = this.state;
+      const { operators } = this;
+      const displayIsZero = () => display == 0;
+      const lastInput = display.toString().charAt(display.length - 1);
 
       switch (true) {
-        case text === "reset":
-          this.state = {
-            display: 0,
-            calculation: 0,
-            records: 0,
-            waiting: false,
-          };
+        case text === 'ac':
+          this.handleDelete();
           break;
-        case text === "ac":
-          if (firstTwoInput === "-(" && lastInput === ")") {
-            this.state.display = removedScopedMinus;
-            return;
-          }
-          if (display.length === 1) {
-            this.state.display = 0;
-            return;
-          }
-          this.state.display = display.trim().slice(0, -1);
+        case text === 'percent':
+          this.handlePercent();
           break;
-        case text === "plusmin":
-          if (lastInput.match(operators)) return;
-          if (firstTwoInput === "-(" && lastInput === ")") {
-            this.state.display = removedScopedMinus;
-          } else if (firstTwoInput.match(/[0-9]/g) && !isOperatorIncluded) {
-            this.state.display = -display;
-          } else {
-            this.state.display = `-(${display})`;
-          }
+        case text === 'plusmin':
+          this.handlePlusMin();
           break;
-        case text === "percent":
-          if (lastInput.match(operators)) {
-            this.state.display = display.replaceAt(display.length - 2, "");
-          }
-
-          this.calculate("percent");
-        case text === "=":
-          if (displayIsZero() || lastIsDot() || lastIsOperator) return;
-          this.calculate();
+        case text === 'reset':
+          this.handleReset();
           break;
-        case text === ".":
-          const numbersOnly = display.split(operators);
-          if (displayIsZero()) {
-            this.state.display += ".";
-            return;
-          }
-          if (includeDot) return;
-          this.state.display += text;
+        case text === '.':
+          this.handleDot();
           break;
         case text.match(operators) !== null:
-          if (displayIsZero() || lastIsDot()) return;
-          if (lastInput.match(operators)) {
-            this.state.display =
-              display.substring(0, display.length - 1) + text;
-            return;
-          }
-          this.state.display += text;
+          this.handleOperator(text);
+          break;
+        case text === '=':
+          this.handleCalculate();
           break;
         default:
-          if (lastInput === ")") return;
+          if (lastInput === ')') return;
           if (displayIsZero()) {
             this.state.records = text;
             this.state.display = text;
@@ -263,29 +206,105 @@ export default {
           break;
       }
     },
-    calculate(mode = "default") {
+    handleCalculate(mode = 'default') {
       let display = this.state.display.toString();
-      display = display.replaceAll("x", "*");
-      display = display.replaceAll("÷", "/");
+      display = display.replaceAll('x', '*');
+      display = display.replaceAll('÷', '/');
+
+      const count = () => new Function(`return ${display}`)();
 
       let result;
       switch (mode) {
-        case "percent":
-          result = parseFloat((eval(display) / 100).toFixed(3));
+        case 'percent':
+          result = parseFloat((count() / 100).toFixed(3));
           break;
         default:
-          result = parseFloat(eval(display).toFixed(3));
+          result = parseFloat(count().toFixed(3));
           this.state.calculation = this.state.display;
           break;
       }
       this.state.display = result;
       this.state.records = 0;
     },
-    reset() {
+    handleDelete() {
+      const display = this.state.display.toString();
+      const firstTwoInput = display.substring(0, 2);
+      const lastInput = display.charAt(display.length - 1);
+      const removedScopedMinus = display.substring(2, display.length - 1);
+      if (firstTwoInput === '-(' && lastInput === ')') {
+        this.state.display = removedScopedMinus;
+        return;
+      }
+      if (display.length === 1) {
+        this.state.display = 0;
+        return;
+      }
+      this.state.display = display.trim().slice(0, -1);
+    },
+    handleDot() {
+      const { display } = this.state;
+      const { operators } = this;
+      const lastInput = display.toString().slice(-1);
+      const displayIsZero = () => display === 0;
+      if (displayIsZero()) {
+        this.state.display += '.';
+        return;
+      }
+
+      const lastNumbers = display.toString().split(operators).slice(-1)[0];
+      if (lastNumbers.includes('.')) return;
+      if (lastInput.includes(')')) return;
+      if (lastInput.match(operators)) return;
+      this.state.display += '.';
+    },
+    handleOperator(text) {
+      const { display } = this.state;
+      const { operators } = this;
+      const lastInput = display.toString().slice(-1);
+      const lastIsDot = () => lastInput === '.';
+      const displayIsZero = () => display == 0;
+
+      if (displayIsZero() || lastIsDot()) return;
+      if (lastInput.match(operators)) {
+        this.state.display = display.substring(0, display.length - 1) + text;
+        return;
+      }
+      this.state.display += text;
+    },
+    handlePercent() {
+      const display = this.state.display.toString();
+      const lastInput = display.charAt(display.length - 1);
+      const { operators } = this;
+      if (lastInput.match(operators)) {
+        this.state.display = display.substring(0, display.length - 1);
+      }
+
+      this.handleCalculate('percent');
+    },
+    handlePlusMin() {
+      const display = this.state.display.toString();
+      const { operators } = this;
+      const isOperatorIncluded = display
+        .substring(1, display.length)
+        .match(operators);
+      const firstTwoInput = display.substring(0, 2);
+      const lastInput = display.charAt(display.length - 1);
+      const removedScopedMinus = display.substring(2, display.length - 1);
+      if (lastInput.match(operators)) return;
+      if (firstTwoInput === '-(' && lastInput === ')') {
+        this.state.display = removedScopedMinus;
+      } else if (firstTwoInput.match(/[0-9]/g) && !isOperatorIncluded) {
+        this.state.display = -display;
+      } else {
+        this.state.display = `-(${display})`;
+      }
+    },
+    handleReset() {
       this.state = {
         display: 0,
         calculation: 0,
         records: 0,
+        waiting: false,
       };
     },
   },
